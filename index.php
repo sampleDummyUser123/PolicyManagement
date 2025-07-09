6a58358dc806a60d215fe7a5357ff738f9e46ac0
REgularNAme <my.atlas.team2024@gmail.com>
2025-07-09 17:53:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // vulnerable.php
 $name = $_GET['name'];
 $other = $_GET['other'];
+$other2 = $_GET['other2'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,6 +16,6 @@ $other = $_GET['other'];
     <h1>Hello, <?php echo $other; ?>!</h1>
     <h1>Hello, <?php echo $name; ?>!</h1>
     <h1>Hello, <?php echo $name; ?>!</h1>
-    <h1>Hello, <?php echo $other; ?>!</h1>
+    <h1>Hello, <?php echo $other2; ?>!</h1>
 </body>
 </html>
